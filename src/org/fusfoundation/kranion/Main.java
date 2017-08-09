c54c1fb68907126213a328df0d4e3421da1346fb
Lindsey Abramson <labramson@fusf.local>
2017-08-09 14:19:42 -0400

    completed functionality

--- src/org/fusfoundation/kranion/Main.java
+++ src/org/fusfoundation/kranion/Main.java
@@ -44,7 +44,6 @@ import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.*;
 
-
 import org.lwjgl.PointerBuffer;
 
 import java.nio.FloatBuffer;
@@ -62,14 +61,11 @@ import static org.lwjgl.opencl.CL10GL.*;
 
 import java.lang.reflect.Constructor;
 
-
-
 import java.io.File;
 import java.awt.event.*;
 import javax.swing.*;
 import java.util.StringTokenizer;
 import java.util.List;
-
 
 import org.fusfoundation.kranion.model.*;
 import org.fusfoundation.kranion.model.image.*;
@@ -92,7 +88,6 @@ import javax.imageio.ImageIO;
 import org.lwjgl.opencl.CL10GL;
 import static org.lwjgl.opengl.GL20.glGetUniformLocation;
 
-
 public class Main implements ProgressListener {
 
     @Override
@@ -103,9 +98,9 @@ public class Main implements ProgressListener {
     public static final int DISPLAY_HEIGHT = 1024;
     public static final int DISPLAY_WIDTH = 1680;
     public static final Logger LOGGER = Logger.getLogger(Main.class.getName());
-    
+
     public static float OpenGLVersion = -1f;
-    
+
     public static final boolean DEBUG = false;
 
     private float viewportAspect = 1f;
@@ -130,10 +125,10 @@ public class Main implements ProgressListener {
 //    private Cylinder mrBore, mrBoreOuter;
 //    private Ring mrBoreFront, mrBoreBack;
 //>>>>>>> a2c7a7e First functional check in. -SDR implemented as a reasonable first pass. -GUI controls are still almost non-existent. -Must restart to load different data.
-    
-    private    int currentBuffer = 0;
-    private    boolean bufferNeedsRendering[] = new boolean[3];
-    
+
+    private int currentBuffer = 0;
+    private boolean bufferNeedsRendering[] = new boolean[3];
+
     static {
         try {
             LOGGER.addHandler(new FileHandler("errors.log", true));
@@ -144,21 +139,19 @@ public class Main implements ProgressListener {
 
     private static Main main;
     private static long mainThreadId = Thread.currentThread().getId();
-        
+
     public static void main(String[] args) {
-        try {            
+        try {
             main = new Main();
-                                    
+
             main.create();
-            
+
             SplashScreen mySplash = SplashScreen.getSplashScreen();
-           
-            
-            
+
             if (mySplash != null) {
                 mySplash.close();
             }
-            
+  
             main.run();
         } catch (Exception ex) {
             LOGGER.log(Level.SEVERE, ex.toString(), ex);
@@ -172,7 +165,7 @@ public class Main implements ProgressListener {
     public Main() {
 
     }
-    
+
     public static void update() {
         // If the caller is in the same thread and thus blocking the main loop,
         // we do one cycle of the main loop without input checking.
@@ -182,36 +175,37 @@ public class Main implements ProgressListener {
         if (Thread.currentThread().getId() == mainThreadId) {
             main.handleResize();
             main.nextFrame();
-            
+
             Main.checkForGLError();
         }
     }
 
     public void create() throws LWJGLException {
-        
+
         model = new Model();
-        
+
         controller = new DefaultController();
         controller.setModel(model);
-        
+
         view = new DefaultView();
-        
+
         view.setModel(model);
         view.setPropertyPrefix("Model.Attribute");
         view.setController(controller);
-        
+
         controller.setView(view);
-              
+
         //Display
         DisplayMode[] modes = Display.getAvailableDisplayModes();
-        
+
         DisplayMode chosenMode = null;
-        for (int i=0; i<modes.length; i++) {
+        for (int i = 0; i < modes.length; i++) {
             DisplayMode current = modes[i];
-            System.out.println(current.getWidth() + "x" + current.getHeight() + "x" +
-                    current.getBitsPerPixel() + " " + current.getFrequency() + "Hz");
-            if (current.getBitsPerPixel() == 32 && current.getWidth() == 2560 && current.getHeight() == 1440 && current.getFrequency() == 60)
+            System.out.println(current.getWidth() + "x" + current.getHeight() + "x"
+                    + current.getBitsPerPixel() + " " + current.getFrequency() + "Hz");
+            if (current.getBitsPerPixel() == 32 && current.getWidth() == 2560 && current.getHeight() == 1440 && current.getFrequency() == 60) {
                 chosenMode = current;
+            }
         }
         DisplayMode mode = new DisplayMode(DISPLAY_WIDTH, DISPLAY_HEIGHT);
 //        mode = chosenMode;
@@ -221,12 +215,12 @@ public class Main implements ProgressListener {
         System.out.println("Display: mode set");
         Display.setResizable(true);
         Display.setTitle("Kranion");
-        
+
         System.out.println("Setting pixel format...");
         PixelFormat pixelFormat = new PixelFormat(24, 8, 24, 8, 1);
         org.lwjgl.opengl.ContextAttribs contextAtribs = new ContextAttribs(2, 1);
         contextAtribs.withForwardCompatible(true);
-        
+
         try {
             ByteBuffer[] list = new ByteBuffer[3];
             InputStream rstm = this.getClass().getResourceAsStream("/org/fusfoundation/kranion/images/icon32.png");
@@ -246,16 +240,15 @@ public class Main implements ProgressListener {
         System.out.println("Creating display...");
         Display.create(pixelFormat, contextAtribs);
         //Display.create();
-    
+
         System.out.println("GL Vendor: " + org.lwjgl.opengl.GL11.glGetString(org.lwjgl.opengl.GL11.GL_VENDOR));
         System.out.println("GL Version: " + org.lwjgl.opengl.GL11.glGetString(org.lwjgl.opengl.GL11.GL_VERSION));
         System.out.println("GLSL Language Version: " + org.lwjgl.opengl.GL11.glGetString(org.lwjgl.opengl.GL20.GL_SHADING_LANGUAGE_VERSION));
         System.out.println("GL Renderer: " + org.lwjgl.opengl.GL11.glGetString(org.lwjgl.opengl.GL11.GL_RENDERER));
 
         checkGLSupport();
-        
+
         checkCLSupport();
-            
 
         //Keyboard
         Keyboard.create();
@@ -281,12 +274,12 @@ public class Main implements ProgressListener {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
+
         resizeGL();
 //        resizeGL();
 
     }
-    
+
     private ByteBuffer convertToByteBuffer(BufferedImage image) {
         byte[] buffer = new byte[image.getWidth() * image.getHeight() * 4];
         int counter = 0;
@@ -318,77 +311,73 @@ public class Main implements ProgressListener {
             System.out.println("CTX created");
 
             IntBuffer errcode_ret = BufferUtils.createIntBuffer(1);
-            
+
             List<CLDevice> devices = platform.getDevices(CL_DEVICE_TYPE_GPU);
-            
+
             System.out.println(devices.size() + " GPU devices found.");
-            
+
             // long context = clCreateContext(platform, devices, null, null, null);
             //CLContext context = org.lwjgl.opencl.CLContext.createFromType(platform, Thread.currentThread().getId(), null, Display.getDrawable(), errcode_ret);
             CLContext context = org.lwjgl.opencl.CLContext.createFromType(platform, CL_DEVICE_TYPE_GPU, null, Display.getDrawable(), errcode_ret);
-            
-                         
-              System.out.println("Device: " + devices.get(0).getInfoString(CL_DEVICE_NAME));
-              System.out.println("Device CL extensions: " + devices.get(0).getInfoString(CL_DEVICE_EXTENSIONS));
+
+            System.out.println("Device: " + devices.get(0).getInfoString(CL_DEVICE_NAME));
+            System.out.println("Device CL extensions: " + devices.get(0).getInfoString(CL_DEVICE_EXTENSIONS));
             System.out.println("CONTEXT created. error code = " + errcode_ret.get(0));
-            
-            
+
             errcode_ret.clear();
-org.lwjgl.opencl.CLCommandQueue queue = org.lwjgl.opencl.CL10.clCreateCommandQueue(context, devices.get(0), org.lwjgl.opencl.CL10.CL_QUEUE_PROFILING_ENABLE, errcode_ret);
-   // checkCLError throw a CLExcepetion if the error code does not equal CL_SUCCESS. This exception should be caught and all currently created resources released. See later. 
-   org.lwjgl.opencl.Util.checkCLError(errcode_ret.get(0));
-   
+            org.lwjgl.opencl.CLCommandQueue queue = org.lwjgl.opencl.CL10.clCreateCommandQueue(context, devices.get(0), org.lwjgl.opencl.CL10.CL_QUEUE_PROFILING_ENABLE, errcode_ret);
+            // checkCLError throw a CLExcepetion if the error code does not equal CL_SUCCESS. This exception should be caught and all currently created resources released. See later. 
+            org.lwjgl.opencl.Util.checkCLError(errcode_ret.get(0));
+
             org.lwjgl.opencl.CL10.clReleaseCommandQueue(queue);
-            org.lwjgl.opencl.CL10.clReleaseContext(context);    
+            org.lwjgl.opencl.CL10.clReleaseContext(context);
             org.lwjgl.opencl.CL.destroy();
             System.out.println("CL context released, CL shutdown");
             System.out.println("****************\n");
-            
+
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("*** Problem initializing OpenCL");
         }
-        
+
     }
-    
+
     private void checkGLSupport() {
         String vendor = org.lwjgl.opengl.GL11.glGetString(org.lwjgl.opengl.GL11.GL_VENDOR);
         String version = org.lwjgl.opengl.GL11.glGetString(org.lwjgl.opengl.GL11.GL_VERSION);
         int nMaxTexUnits = org.lwjgl.opengl.GL11.glGetInteger(org.lwjgl.opengl.GL20.GL_MAX_TEXTURE_IMAGE_UNITS);
         int nMaxCombinedTexUnits = org.lwjgl.opengl.GL11.glGetInteger(org.lwjgl.opengl.GL20.GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS);
-        
+
         try {
-        float versionVal = 0f;
-        System.out.println("  Texture unit count = " + nMaxTexUnits + ", Max combined textures = " + nMaxCombinedTexUnits);
-        StringTokenizer tok = new StringTokenizer(version, ". ");
-        if (tok.hasMoreElements()) {
-            versionVal = Float.parseFloat(tok.nextToken());
-        }
-        if (tok.hasMoreElements()) {
-            versionVal += Float.parseFloat(tok.nextToken())/10f;
-        }
-        
-        Main.OpenGLVersion = versionVal;
-        
-        if (versionVal < 4.5f) {
-            JOptionPane.showMessageDialog(null, "OpenGL 4.5 or later required.\n\nYou have:\n" + vendor + "\n" + version);
+            float versionVal = 0f;
+            System.out.println("  Texture unit count = " + nMaxTexUnits + ", Max combined textures = " + nMaxCombinedTexUnits);
+            StringTokenizer tok = new StringTokenizer(version, ". ");
+            if (tok.hasMoreElements()) {
+                versionVal = Float.parseFloat(tok.nextToken());
+            }
+            if (tok.hasMoreElements()) {
+                versionVal += Float.parseFloat(tok.nextToken()) / 10f;
+            }
+
+            Main.OpenGLVersion = versionVal;
+
+            if (versionVal < 4.5f) {
+                JOptionPane.showMessageDialog(null, "OpenGL 4.5 or later required.\n\nYou have:\n" + vendor + "\n" + version);
 //            System.exit(1);
-        }
-        }
-        catch(Exception e) {
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
-                
 
     }
-    
+
     public void destroy() {
         //Methods already check if created before destroying.
         Mouse.destroy();
         Keyboard.destroy();
         Display.destroy();
     }
-    
+
     //----------- Variables added for Lighting Test -----------//
     private FloatBuffer matSpecular;
     private FloatBuffer lightPosition;
@@ -422,39 +411,39 @@ org.lwjgl.opencl.CLCommandQueue queue = org.lwjgl.opencl.CL10.clCreateCommandQue
 //        glHint(GL_POLYGON_SMOOTH_HINT, GL_NICEST);
         glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
         glShadeModel(GL_SMOOTH);
-        glMaterial(GL_FRONT_AND_BACK, GL_SPECULAR, matSpecular);				// sets specular material color
-        glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 50.0f);					// sets shininess
+        glMaterial(GL_FRONT_AND_BACK, GL_SPECULAR, matSpecular);  // sets specular material color
+        glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 50.0f);  // sets shininess
 
         // lighting setup
-        glLight(GL_LIGHT0, GL_POSITION, lightPosition);				// sets light position
-        glLight(GL_LIGHT0, GL_SPECULAR, whiteLight);				// sets specular light to white
-        glLight(GL_LIGHT0, GL_DIFFUSE, whiteLight);					// sets diffuse light to white
-        glLightModel(GL_LIGHT_MODEL_AMBIENT, lModelAmbient);		// global ambient light 
-        glEnable(GL_LIGHTING);										// enables lighting
+        glLight(GL_LIGHT0, GL_POSITION, lightPosition);  // sets light position
+        glLight(GL_LIGHT0, GL_SPECULAR, whiteLight);  // sets specular light to white
+        glLight(GL_LIGHT0, GL_DIFFUSE, whiteLight);  // sets diffuse light to white
+        glLightModel(GL_LIGHT_MODEL_AMBIENT, lModelAmbient);  // global ambient light 
+        glEnable(GL_LIGHTING);  // enables lighting
         // enables light0
         glEnable(GL_LIGHT0);
 
         glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE);
         glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE);
 
-        glEnable(GL_COLOR_MATERIAL);								// enables opengl to use glColor3f to define material color
-        glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);	// tell opengl glColor3f effects the ambient and diffuse properties of material
-        
-        
+        glEnable(GL_COLOR_MATERIAL);  // enables opengl to use glColor3f to define material color
+        glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);  // tell opengl glColor3f effects the ambient and diffuse properties of material
+
     }
-    
+
     public void resizeGL() throws org.lwjgl.LWJGLException {
         //2D Scene
         System.out.println("Viewport: " + Display.getWidth() + ", " + Display.getHeight());
-        
-        if (Display.getWidth() <=0 || Display.getHeight() <= 0)  return;
-        
+
+        if (Display.getWidth() <= 0 || Display.getHeight() <= 0) {
+            return;
+        }
+
         glViewport(0, 0, Display.getWidth(), Display.getHeight());
-        
+
 //        trackball.set(Display.getWidth() / 2, Display.getHeight() / 2, Display.getHeight() / 2f);
 //        registerBall.set(Display.getWidth() / 2, Display.getHeight() / 2, Display.getHeight() / 2f);
 //        registerBall2.set(Display.getWidth() / 2, Display.getHeight() / 2, Display.getHeight() / 2f);
-
         glMatrixMode(GL_PROJECTION);
         glLoadIdentity();
         viewportAspect = (float) Display.getWidth() / (float) Display.getHeight();
@@ -462,39 +451,38 @@ org.lwjgl.opencl.CLCommandQueue queue = org.lwjgl.opencl.CL10.clCreateCommandQue
 
         glMatrixMode(GL_MODELVIEW);
         glLoadIdentity();
-        
+
         view.doLayout();
     }
 
-    public void run() {        
-        while (!Display.isCloseRequested() /*&& !Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)*/) { 
+    public void run() {
+        while (!Display.isCloseRequested() /*&& !Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)*/) {
 
-                handleResize();
-                
-                view.processInput();                
-                
-                nextFrame();
-                
-                Main.checkForGLError();
-                
+            handleResize();
+
+            view.processInput();
+
+            nextFrame();
+
+            Main.checkForGLError();
+
         }
-        
+
         view.release();
 
     }
-    
+
     public void handleResize() {
         if (Display.wasResized()) {
             try {
                 resizeGL();
-            }
-            catch(org.lwjgl.LWJGLException e) {
+            } catch (org.lwjgl.LWJGLException e) {
                 System.out.println(e);
                 System.exit(0);
             }
-        }        
+        }
     }
-    
+
     public void nextFrame() {
         if (Display.isVisible()) {
             // manage rendering into double buffer
@@ -514,35 +502,35 @@ org.lwjgl.opencl.CLCommandQueue queue = org.lwjgl.opencl.CL10.clCreateCommandQue
         Display.processMessages();
         Display.update();
         Display.sync(60);
-        currentBuffer = (currentBuffer+1)%3; // keep track of front/back buffers        
+        currentBuffer = (currentBuffer + 1) % 3; // keep track of front/back buffers        
     }
-        
+
     public static void checkForGLErrorAndThrow() {
         int error = checkForGLError();
         if (error != GL_NO_ERROR) {
             throw new org.lwjgl.opengl.OpenGLException(error);
-        }        
+        }
     }
-    
+
     public static void printStackTrace() {
         StackTraceElement[] traces;
         traces = Thread.currentThread().getStackTrace();
-        for (int i=2; i<traces.length; i++) {
+        for (int i = 2; i < traces.length; i++) {
             System.out.println("\t" + traces[i]);
         }
     }
-    
+
     public static int checkForGLError() {
         int error = glGetError();
         if (error != GL_NO_ERROR) {
             System.out.println("GL ERROR DETECTED.");
-            switch(error) {
+            switch (error) {
                 case GL_INVALID_ENUM:
                     System.out.println("GL_INVALID_ENUM");
                     break;
                 case GL_INVALID_VALUE:
                     System.out.println("GL_INVALID_VALUE");
-                     break;
+                    break;
                 case GL_INVALID_OPERATION:
                     System.out.println("GL_INVALID_OPERATION");
                     break;
@@ -565,7 +553,7 @@ org.lwjgl.opencl.CLCommandQueue queue = org.lwjgl.opencl.CL10.clCreateCommandQue
         }
         return error;
     }
-    
+
     public static void glPushMatrix() {
         org.lwjgl.opengl.GL11.glPushMatrix();
 
@@ -576,7 +564,7 @@ org.lwjgl.opencl.CLCommandQueue queue = org.lwjgl.opencl.CL10.clCreateCommandQue
             System.out.println("PROJECTIONVIEW max stack depth: " + glGetInteger(GL_MAX_PROJECTION_STACK_DEPTH));
         }
     }
-    
+
     public static void glPopMatrix() {
         org.lwjgl.opengl.GL11.glPopMatrix();
 
@@ -587,16 +575,16 @@ org.lwjgl.opencl.CLCommandQueue queue = org.lwjgl.opencl.CL10.clCreateCommandQue
             System.out.println("PROJECTIONVIEW max stack depth: " + glGetInteger(GL_MAX_PROJECTION_STACK_DEPTH));
         }
     }
-    
+
     public static void glPushAttrib(int bitmask) {
         org.lwjgl.opengl.GL11.glPushAttrib(bitmask);
-        
+
         if (DEBUG && Main.checkForGLError() != GL_NO_ERROR) {
             System.out.println("ATTRIB stack depth: " + glGetInteger(GL_ATTRIB_STACK_DEPTH));
             System.out.println("ATTRIB max stack depth: " + glGetInteger(GL_MAX_ATTRIB_STACK_DEPTH));
         }
     }
-    
+
     public static void glPopAttrib() {
         org.lwjgl.opengl.GL11.glPopAttrib();
 
@@ -605,5 +593,5 @@ org.lwjgl.opencl.CLCommandQueue queue = org.lwjgl.opencl.CL10.clCreateCommandQue
             System.out.println("ATTRIB max stack depth: " + glGetInteger(GL_MAX_ATTRIB_STACK_DEPTH));
         }
     }
-    
+
 }
